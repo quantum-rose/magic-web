8160031095e9e261ccecd4d7ca465d8ededf7cdd
quantum rose <cui-haojie@outlook.com>
2020-07-09 19:05:49 +0800

    新拟态风格计算器

--- 新拟态风格计算器/script.js
+++ 新拟态风格计算器/script.js
@@ -3,37 +3,69 @@ import 'https://cdn.staticfile.org/vue/2.6.11/vue.js';
 new Vue({
     el: '#calculator',
     data: {
-        result: '0',
+        expression: '0',
+        isCalculated: false,
+    },
+    computed: {
+        result() {
+            return this.expression === 'Infinity'
+                ? '不能除以0'
+                : this.expression.replace(/\d+(\.\d*)?/g, match =>
+                      match.replace(/(?<=^\d+)(?=(\d{3})+$|(\d{3})+\.)/g, ',')
+                  );
+        },
     },
     methods: {
         clear() {
-            this.result = '0';
+            this.expression = '0';
         },
         toggleSign() {
-            this.result += '* -1';
-            this.calculate();
+            if (/\d/.test(this.expression.slice(-1))) {
+                this.expression = this.expression.replace(/\d+(\.\d*)?$/g, match => (match * -1).toFixed(10) - 0 + '');
+            }
         },
-        percentage() {},
+        percentage() {
+            if (/\d/.test(this.expression.slice(-1))) {
+                this.expression = this.expression.replace(
+                    /\d+(\.\d*)?$/g,
+                    match => (match * 0.01).toFixed(10) - 0 + ''
+                );
+            }
+        },
         append(character) {
-            let lastWord = this.result.slice(-1);
-            if (/[0-9]/.test(character)) {
-                if (lastWord !== '0') {
-                    this.result += character;
+            this.expression === 'Infinity' && this.clear();
+            if (character === 0) {
+                if (!/^0$|[+\-×÷]0$/.test(this.expression)) {
+                    this.expression += '0';
+                }
+            } else if (/[1-9]/.test(character)) {
+                if (this.expression === '0' || this.isCalculated) {
+                    this.expression = character + '';
+                    this.isCalculated = false;
                 } else {
-                    this.result = '' + character;
+                    this.expression += character;
+                }
+            } else if (/[+\-×÷]/.test(character)) {
+                this.isCalculated = false;
+                if (/[+\-×÷]/.test(this.expression.slice(-1))) {
+                    this.expression = this.expression.slice(0, -1) + character;
+                } else {
+                    this.expression += character;
                 }
             } else {
-                if (/\+|-|×|÷|\./.test(lastWord)) {
-                    if (lastWord !== character) {
-                        this.result = this.result.slice(0, -1) + character;
-                    }
-                } else {
-                    this.result += character;
+                if (/^\d+$|[+\-×÷]\d+$/.test(this.expression)) {
+                    this.isCalculated = false;
+                    this.expression += '.';
+                } else if (/[+\-×÷]/.test(this.expression.slice(-1))) {
+                    this.expression += '0.';
                 }
             }
         },
         calculate() {
-            this.result = new Function(`return ${this.result.replace('×', '*').replace('÷', '/')}`)() + '';
+            if (/[\d.]/.test(this.expression.slice(-1))) {
+                this.expression = eval(this.expression.replace(/×/g, '*').replace(/÷/g, '/')).toFixed(10) - 0 + '';
+                this.isCalculated = true;
+            }
         },
     },
 });
