ed15ddf1903bcb745f0e569d0f92445b52022179
quantum-rose <cui-haojie@outlook.com>
2020-07-29 00:45:15 +0800

    continue

--- 16-CSS滤镜/script.js
+++ 16-CSS滤镜/script.js
@@ -11,16 +11,34 @@ new Vue({
             },
             resistance: true,
             resistanceRatio: 0,
-            noSwipingSelector: 'input',
+            noSwiping: true,
         },
         blur: 0,
         brightness: 1,
         contrast: 1,
-        dropShadow: { x: 0, y: 0, blur: 0, color: '#000000' },
+        dropShadow: { x: 0, y: 0, blur: 0, color: '#ffffff' },
+        grayscale: 0,
+        hueRotate: 0,
+        invert: 0,
+        opacity: 1,
+        saturate: 1,
+        sepia: 0,
     },
     computed: {
         filterStyle() {
-            return `blur(${this.blur}px) brightness(${this.brightness}) contrast(${this.contrast})`;
+            const {
+                blur,
+                brightness,
+                contrast,
+                dropShadow,
+                grayscale,
+                hueRotate,
+                invert,
+                opacity,
+                saturate,
+                sepia,
+            } = this;
+            return `blur(${blur}px) brightness(${brightness}) contrast(${contrast}) drop-shadow(${dropShadow.x}px ${dropShadow.y}px ${dropShadow.blur}px ${dropShadow.color}) grayscale(${grayscale}) hue-rotate(${hueRotate}deg) invert(${invert}) opacity(${opacity}) saturate(${saturate}) sepia(${sepia})`;
         },
     },
 });
