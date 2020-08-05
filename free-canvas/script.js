8160bad1251d6c85460f9d2a1d1229e1560fc06f
quantum rose <cui-haojie@outlook.com>
2020-08-05 18:51:21 +0800

    free-canvas一个可自由拖动与缩放的画布

---
+++ free-canvas/script.js
@@ -0,0 +1,187 @@
+import Vector2D from '../libs/Vector2D.js';
+import parametric from '../libs/parametric.js';
+
+const $cvs = document.querySelector('#cvs');
+const $reset = document.querySelector('.reset');
+const cvsCtx = $cvs.getContext('2d');
+let w = ($cvs.width = $cvs.offsetWidth * window.devicePixelRatio);
+let h = ($cvs.height = $cvs.offsetHeight * window.devicePixelRatio);
+let offsetX = w / 2; // 画布坐标原点的偏移，初始为画布大小的一半，使得原点位于屏幕中心
+let offsetY = h / 2;
+let mouseX = 0; // 鼠标相对于画布原点的坐标
+let mouseY = 0;
+let zoom = 1; // 画布缩放倍数
+let lastPoint = {
+    x: 0,
+    y: 0,
+}; // 鼠标移动过程中上一个点的位置
+
+/**
+ * 监听窗口大小的改变，动态修改canvas的大小
+ */
+window.addEventListener('resize', function () {
+    w = $cvs.width = $cvs.offsetWidth * window.devicePixelRatio;
+    h = $cvs.height = $cvs.offsetHeight * window.devicePixelRatio;
+    offsetX = w / 2;
+    offsetY = h / 2;
+});
+
+/**
+ * 绑定滚轮事件，处理兼容性问题
+ * @param {Element} elem
+ * @param {Function} callback
+ */
+function bindWheelEvent(elem, callback) {
+    if (window.navigator.userAgent.toLowerCase().indexOf('firefox') != -1) {
+        elem.addEventListener('DOMMouseScroll', wheel, false);
+    } else {
+        elem.addEventListener('mousewheel', wheel, false);
+    }
+    function wheel(e) {
+        e.preventDefault();
+        const dir = e.detail ? e.detail > 0 : e.wheelDelta < 0;
+        callback(e, dir);
+    }
+}
+
+/**
+ * 根据滚轮滚动方向缩放画布，并且是以鼠标所在位置为中心进行缩放
+ */
+bindWheelEvent($cvs, function (e, dir) {
+    mouseX = e.offsetX - offsetX;
+    mouseY = offsetY - e.offsetY;
+
+    if (dir && zoom > 0.1) {
+        zoom *= 0.9;
+        offsetX += mouseX - mouseX * 0.9;
+        offsetY += mouseY * 0.9 - mouseY;
+    }
+
+    if (!dir && zoom < 100) {
+        zoom /= 0.9;
+        offsetX += mouseX - mouseX / 0.9;
+        offsetY += mouseY / 0.9 - mouseY;
+    }
+});
+
+function grabbing(e) {
+    const { offsetX: x, offsetY: y } = e;
+    offsetX += x - lastPoint.x;
+    offsetY += y - lastPoint.y;
+    lastPoint.x = x;
+    lastPoint.y = y;
+}
+function removeEvent() {
+    $cvs.removeEventListener('mousemove', grabbing);
+    $cvs.removeEventListener('mouseleave', removeEvent);
+}
+function grab(e) {
+    lastPoint.x = e.offsetX;
+    lastPoint.y = e.offsetY;
+    $cvs.addEventListener('mousemove', grabbing);
+    $cvs.addEventListener('mouseleave', removeEvent);
+}
+$cvs.addEventListener('mousedown', grab);
+$cvs.addEventListener('mouseup', removeEvent);
+
+$reset.addEventListener('click', function () {
+    offsetX = w / 2;
+    offsetY = h / 2;
+    zoom = 1;
+});
+
+/**
+ * 圆
+ */
+const arc = parametric((t, cx, cy, r) => [cx + r * Math.cos(t), cy + r * Math.sin(t)]);
+
+/**
+ * 圆心(0,0)半径100的圆
+ */
+const arc_0_0_100 = parametric(t => [100 * Math.cos(t), 100 * Math.sin(t)]);
+
+/**
+ * 椭圆
+ */
+const ellipse = parametric((t, cx, cy, rx, ry) => [cx + rx * Math.cos(t), cy + ry * Math.sin(t)]);
+
+/**
+ * 抛物线
+ */
+const parabola = parametric((t, x, y, p) => [x + 2 * p * t, y + 2 * p * t ** 2]);
+
+/**
+ * 阿基米德螺线
+ */
+const helical = parametric((t, l) => [l * t * Math.cos(t), l * t * Math.sin(t)]);
+
+/**
+ * 四角星
+ */
+const star = parametric((t, l) => [l * Math.cos(t) ** 3, l * Math.sin(t) ** 3]);
+
+/**
+ * 二次贝塞尔曲线
+ */
+const quadricBezier = parametric((t, p0, p1, p2) => [
+    (1 - t) ** 2 * p0.x + 2 * (1 - t) * t * p1.x + t ** 2 * p2.x,
+    (1 - t) ** 2 * p0.y + 2 * (1 - t) * t * p1.y + t ** 2 * p2.y,
+]);
+
+/**
+ * 三次贝塞尔曲线
+ */
+const cubicBezier = parametric((t, p0, p1, p2, p3) => [
+    (1 - t) ** 3 * p0.x + 3 * (1 - t) ** 2 * t * p1.x + 3 * (1 - t) * t ** 2 * p2.x + t ** 3 * p3.x,
+    (1 - t) ** 3 * p0.y + 3 * (1 - t) ** 2 * t * p1.y + 3 * (1 - t) * t ** 2 * p2.y + t ** 3 * p3.y,
+]);
+
+requestAnimationFrame(function render() {
+    cvsCtx.clearRect(0, 0, w, h);
+    cvsCtx.save();
+    cvsCtx.transform(zoom, 0, 0, -zoom, offsetX, offsetY);
+
+    const per = (Date.now() % 2500) / 2500;
+    const firstPer = Math.min(per / 0.5, 1);
+    const secondPer = Math.max((per - 0.5) / 0.5, 0);
+    const offset = Math.PI * 2 * ((Date.now() % 3000) / 3000);
+
+    arc(0, Math.PI * 2, 60, -800, 800, 200).draw(cvsCtx);
+    arc(secondPer * Math.PI * 2 + offset, firstPer * Math.PI * 2 + offset, 60, 800, 800, 200).draw(cvsCtx, {
+        lineWidth: 30,
+        strokeStyle: '#007bff',
+    });
+
+    arc_0_0_100(0, Math.PI * 2).draw(cvsCtx);
+
+    ellipse(0, Math.PI * 2, 60, -800, -800, 300, 200).draw(cvsCtx);
+    ellipse(secondPer * Math.PI * 2 + offset, firstPer * Math.PI * 2 + offset, 60, 800, -800, 300, 50).draw(cvsCtx);
+
+    parabola(-10, 10, 800, 0, 0, 100).draw(cvsCtx);
+
+    helical(0, 50, 800, 10).draw(cvsCtx, { strokeStyle: 'red' });
+
+    star(0, Math.PI * 2, 100, 400).draw(cvsCtx, { strokeStyle: 'blue' });
+
+    {
+        const p0 = new Vector2D(-1200, 0);
+        const p1 = new Vector2D(-800, 0);
+        p1.rotate(Math.PI / 4);
+        const p2 = new Vector2D(-750, 0);
+        quadricBezier(0, 1, 100, p0, p1, p2).draw(cvsCtx);
+    }
+
+    {
+        const p0 = new Vector2D(600, 0);
+        const p1 = new Vector2D(1100, 0);
+        p1.rotate(Math.PI / -4);
+        const p2 = new Vector2D(1200, 0);
+        p2.rotate(Math.PI / 4);
+        const p3 = new Vector2D(1250, 0);
+        cubicBezier(0, 1, 100, p0, p1, p2, p3).draw(cvsCtx);
+    }
+
+    cvsCtx.restore();
+
+    requestAnimationFrame(render);
+});
--- libs/parametric.js
+++ libs/parametric.js
@@ -1,3 +1,9 @@
+/**
+ * 依次连接坐标点绘制折线
+ * @param {Array[Array]} points 坐标点集合，每一项为长度2的数组，两项分别为x和y坐标
+ * @param {CanvasRenderingContext2D} cvsCtx canvas 2d绘图上下文
+ * @param {Object} param2 线条样式
+ */
 function draw(points, cvsCtx, { lineWidth = 1, strokeStyle = 'black', fillStyle = null, close = false } = {}) {
     cvsCtx.save();
     cvsCtx.lineWidth = lineWidth;
@@ -16,6 +22,11 @@ function draw(points, cvsCtx, { lineWidth = 1, strokeStyle = 'black', fillStyle 
     cvsCtx.restore();
 }
 
+/**
+ * 构建曲线参数方程
+ * @param {Function} func 该函数的第一个参数是t，约定返回值是长度为2的数组，数组的两个元素为一个点的x和y坐标
+ * @return {Function} 构造出的曲线函数，前两个参数为t的取值范围，闭区间；第三个参数为区间内的采样点数量，值越大、曲线越平滑；返回值为一个对象，包括一个points数组，为所有采样点的坐标（共有采样点数量+1个坐标点），以及一个draw方法，同上面的draw函数
+ */
 function parametric(func) {
     return function (start, end, segments = 100, ...args) {
         const points = [];
