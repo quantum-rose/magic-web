8d5c883c20b094aac507a590327036b681fb283d
quantum rose <cui-haojie@outlook.com>
2020-08-11 13:49:38 +0800

    完善一些边界处理，加入input控件展示缓存能力

--- sp-06-virtual-list/script.js
+++ sp-06-virtual-list/script.js
@@ -18,9 +18,11 @@ class ListItem {
 }
 
 class VirtualList {
-    constructor(elem, listItems) {
+    constructor(elem, listItems, itemHeight) {
         this.$root = elem;
         this.listItems = listItems;
+        this.itemHeight = itemHeight;
+
         this.$startOffset = document.createElement('div');
         this.$startOffset.classList.add('start-offset');
         this.$root.appendChild(this.$startOffset);
@@ -32,22 +34,18 @@ class VirtualList {
         this.$root.appendChild(this.$endOffset);
 
         this.height = this.$root.offsetHeight;
-        this.visibleCount = Math.ceil(this.height / 120) + 2;
-        window.addEventListener('resize', () => {
-            this.height = this.$root.offsetHeight;
-            this.visibleCount = Math.ceil(this.height / 120) + 2;
-        });
+        this.visibleCount = Math.ceil(this.height / this.itemHeight) + 1; // 能看到的列表项数量
 
-        this.from = Math.floor(this.$root.scrollTop / 120);
-        this.to = Math.min(this.from + this.visibleCount - 1, this.listItems.length);
-        this.startOffset = this.from * 120 + 'px';
+        this.from = Math.floor(this.$root.scrollTop / this.itemHeight); // 起始项，第一项为0
+        this.to = Math.min(this.from + this.visibleCount, this.listItems.length) - 1; // 结束项（包含），最大为length-1
+        this.startOffset = this.from * this.itemHeight + 'px'; // 前偏移，刚好为起始项索引乘单项的高度
         this.$startOffset.style.height = this.startOffset;
-        this.endOffset = (this.listItems.length - this.to) * 120 + 'px';
+        this.endOffset = (this.listItems.length - this.to - 1) * this.itemHeight + 'px'; // 后偏移，为(总数-结束项索引-1)乘单项高度
         this.$endOffset.style.height = this.endOffset;
-        this.visibleListItems = this.listItems.slice(this.from, this.to + 1);
+        this.visibleListItems = this.listItems.slice(this.from, this.to + 1); // slice截取数组，不包含第二个参数所指下标项，所以是结束项索引+1
 
         for (let i = 0; i < this.listItems.length; i++) {
-            if (i >= this.from && i < this.to) {
+            if (i >= this.from && i <= this.to) {
                 if (!this.listItems[i].visible) {
                     this.listItems[i].visible = true;
                     this.$list.append(this.listItems[i].node);
@@ -61,20 +59,21 @@ class VirtualList {
         }
 
         $virtualList.addEventListener('scroll', e => {
-            const _from = Math.floor($virtualList.scrollTop / 120);
+            const _from = Math.floor($virtualList.scrollTop / this.itemHeight);
             if (_from !== this.from) {
                 const flag = _from > this.from;
                 this.from = _from;
-                this.to = Math.min(this.from + this.visibleCount - 1, this.listItems.length);
-                this.startOffset = this.from * 120 + 'px';
+                this.to = Math.min(this.from + this.visibleCount, this.listItems.length) - 1;
+                this.startOffset = this.from * this.itemHeight + 'px';
                 this.$startOffset.style.height = this.startOffset;
-                this.endOffset = (this.listItems.length - this.to) * 120 + 'px';
+                this.endOffset = (this.listItems.length - this.to - 1) * this.itemHeight + 'px';
                 this.$endOffset.style.height = this.endOffset;
                 this.visibleListItems = this.listItems.slice(this.from, this.to + 1);
 
                 if (flag) {
+                    // 向下滚动时，正序遍历，将新的节点插入到列表尾部
                     for (let i = 0; i < this.listItems.length; i++) {
-                        if (i >= this.from && i < this.to) {
+                        if (i >= this.from && i <= this.to) {
                             if (!this.listItems[i].visible) {
                                 this.listItems[i].visible = true;
                                 this.$list.append(this.listItems[i].node);
@@ -87,8 +86,9 @@ class VirtualList {
                         }
                     }
                 } else {
+                    // 向上滚动时，倒序遍历，将新的节点插入到列表头部
                     for (let i = this.listItems.length - 1; i >= 0; i--) {
-                        if (i >= this.from && i < this.to) {
+                        if (i >= this.from && i <= this.to) {
                             if (!this.listItems[i].visible) {
                                 this.listItems[i].visible = true;
                                 this.$list.prepend(this.listItems[i].node);
@@ -112,23 +112,24 @@ class VirtualList {
         if (index > this.listItems.length - 1) {
             index = this.listItems.length - 1;
         }
-        this.$root.scrollTop = index * 120;
+        this.$root.scrollTop = index * this.itemHeight;
     }
 }
 
 const listItems = [];
-
 for (let i = 0; i < 100000; i++) {
     listItems[i] = new ListItem(`
         <div class="container">
             <div class="img"></div>
-            <div class="text">${i}</div>
+            <div class="text">
+                <input type="text" placeholder="${i}" />
+            </div>
         </div>
     `);
 }
 
 const $virtualList = document.querySelector('.virtual-list');
-const virtualList = new VirtualList($virtualList, listItems);
+const virtualList = new VirtualList($virtualList, listItems, 120);
 
 const $input = document.querySelector('#index');
 const $go = document.querySelector('.go');
