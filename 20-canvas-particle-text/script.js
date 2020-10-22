f44477df48a08e398381fd3e8efb9db51c1e0f4e
quantum rose <cui-haojie@outlook.com>
2020-10-22 18:10:09 +0800

    添加文字编辑功能，优化视觉效果

--- 20-canvas-particle-text/script.js
+++ 20-canvas-particle-text/script.js
@@ -1,16 +1,14 @@
 const cvs = document.querySelector('#cvs');
 cvs.width = cvs.offsetWidth * window.devicePixelRatio;
 cvs.height = cvs.offsetHeight * window.devicePixelRatio;
-window.addEventListener('resize', function () {
-    cvs.width = cvs.offsetWidth * window.devicePixelRatio;
-    cvs.height = cvs.offsetHeight * window.devicePixelRatio;
-});
 
 /* 粒子 */
 class Particle {
     cvsCtx = null; // canvas 绘图上下文
     radius = 0; // 粒子半径
     diameter = 0; // 粒子直径
+    minX = 0; // 粒子分布最小坐标
+    minY = 0; // 粒子分布最小坐标
     maxX = 0; // 粒子分布最大坐标
     maxY = 0; // 粒子分布最大坐标
 
@@ -34,18 +32,20 @@ class Particle {
     startTime = 0;
 
     get randomX() {
-        const { maxX, diameter } = this;
-        return (Math.floor((maxX * Math.random()) / diameter) + 0.5) * diameter;
+        const { minX, maxX, diameter } = this;
+        return (Math.floor((Math.random() * (maxX - minX) + minX) / diameter) + 0.5) * diameter;
     }
 
     get randomY() {
-        const { maxY, diameter } = this;
-        return (Math.floor((maxY * Math.random()) / diameter) + 0.5) * diameter;
+        const { minY, maxY, diameter } = this;
+        return (Math.floor((Math.random() * (maxY - minY) + minY) / diameter) + 0.5) * diameter;
     }
 
     constructor({
         cvsCtx,
         radius,
+        minX,
+        minY,
         maxX,
         maxY,
         color = 0,
@@ -57,6 +57,8 @@ class Particle {
         this.cvsCtx = cvsCtx;
         this.radius = radius;
         this.diameter = radius * 2;
+        this.minX = minX;
+        this.minY = minY;
         this.maxX = maxX;
         this.maxY = maxY;
 
@@ -70,10 +72,11 @@ class Particle {
         this.duration = duration;
     }
 
-    update({ random, delay, color, alpha, x, y } = {}) {
+    update({ random, delay, duration, color, alpha, x, y } = {}) {
         this.path.push({
             random: random === undefined ? this.random : random,
             delay: delay === undefined ? this.delay : delay,
+            duration: duration === undefined ? this.duration : duration,
             color: color === undefined ? this.color : color,
             alpha: alpha === undefined ? this.alpha : alpha,
             x: x === undefined ? this.randomX : x + this.radius,
@@ -97,16 +100,11 @@ class Particle {
             cvsCtx,
             radius,
         } = this;
-        const p = this._easeInOutQuad(
-            Math.max(
-                Math.min((Date.now() - startTime - delay) / duration, 1),
-                0
-            )
-        );
-        this.x = (toX - fromX) * p + fromX;
-        this.y = (toY - fromY) * p + fromY;
+        const p = this._easeInOutQuad(Math.max(Math.min((Date.now() - startTime - delay) / duration, 1), 0));
         this.color = (toColor - fromColor) * p + fromColor;
         this.alpha = (toAlpha - fromAlpha) * p + fromAlpha;
+        this.x = (toX - fromX) * p + fromX;
+        this.y = (toY - fromY) * p + fromY;
 
         cvsCtx.beginPath();
         cvsCtx.fillStyle = `hsla(${this.color}, 100%, 50%, ${this.alpha})`;
@@ -117,22 +115,20 @@ class Particle {
         if (p === 1) {
             if (this.path.length === 0) {
                 if (this.random) {
-                    this.update({
-                        random: true,
-                        delay: Math.random() * 1000,
-                    });
+                    this.update({ random: true });
                 } else {
                     return;
                 }
             }
             this.startTime = Date.now();
-            const { random, delay, color, alpha, x, y } = this.path.shift();
+            const { random, delay, duration, color, alpha, x, y } = this.path.shift();
             this.fromColor = this.color;
             this.fromAlpha = this.alpha;
             this.fromX = this.x;
             this.fromY = this.y;
             this.random = random;
             this.delay = delay;
+            this.duration = duration;
             this.toColor = color;
             this.toAlpha = alpha;
             this.toX = x;
@@ -148,16 +144,38 @@ class Particle {
 /* 粒子文本 */
 class ParticleText {
     particles = []; // 所有粒子
-    text = ''; // 当前显示的文字
+    text = []; // 文字轮播队列
     cvs = null; // 粒子画布
     cvsCtx = null; // 粒子画布绘图上下文
     textCvs = null; // 文本画布
     textCvsCtx = null; // 文本画布绘图上下文
     textData = null; // 文本画布的imageData
     cvsRatio = 10; // 粒子画布与文本画布边长之比
+    _spreadTimer = null; // 粒子自动扩散延时器
 
-    constructor(cvs, text = '23:59:59') {
+    get randomText() {
+        return new Date().toTimeString().match(/^.*(?=GMT)/g)[0];
+    }
+
+    get randomColor() {
+        return Math.random() * 360;
+    }
+
+    get randomDelay() {
+        return Math.random() * 1000;
+    }
+
+    get randomDuration() {
+        return 1000 + Math.random() * 1000;
+    }
+
+    get randomAlpha() {
+        return Math.random() > 0.9 ? 0.5 : 0;
+    }
+
+    constructor(cvs, text = '') {
         this.cvs = cvs;
+        this.text.push(text || this.randomText);
         this.cvsCtx = cvs.getContext('2d');
 
         this.textCvs = document.createElement('canvas');
@@ -166,96 +184,106 @@ class ParticleText {
         this.textCvsCtx = this.textCvs.getContext('2d');
 
         this._initParticles();
-        this.updateText(text);
+        this._update();
         this._onEnterFrame();
     }
 
     _initParticles() {
         const {
             textCvs: { width, height },
-            cvs: { width: maxX, height: maxY },
+            cvs: { width: w, height: h },
             cvsCtx,
             cvsRatio,
         } = this;
-        const color = Math.random() * 360;
+        const color = this.randomColor;
         for (let i = 0; i < (width * height) / 10; i++) {
             this.particles[i] = new Particle({
                 cvsCtx,
                 radius: cvsRatio / 2,
-                maxX,
-                maxY,
-                delay: Math.random() * 1000,
+                minX: 0,
+                minY: 0,
+                maxX: w,
+                maxY: h,
+                delay: this.randomDelay,
+                duration: this.randomDuration,
                 color,
+                alpha: this.randomAlpha,
             });
         }
     }
 
-    /**
-     * 更新要显示的文字
-     * @param {String} text 显示的文字
-     */
     updateText(text) {
-        if (text !== this.text) {
-            this.text = text;
-
-            const { particles, textCvsCtx: cvsCtx } = this;
-            const { width, height } = this.textCvs;
-
-            cvsCtx.clearRect(0, 0, width, height);
-            cvsCtx.fillStyle = '#000000';
-            cvsCtx.font = `700 ${width / 6}px Helvetica`;
-            cvsCtx.textAlign = 'center';
-            cvsCtx.textBaseline = 'middle';
-            cvsCtx.fillText(text, width / 2, height / 2);
-
-            const textData = (this.textData = cvsCtx.getImageData(
-                0,
-                0,
-                width,
-                height
-            ));
-
-            // 组成文字的粒子
-            const textParticle = [];
-            for (let i = 0; i < textData.data.length; i += 4) {
-                if (textData.data[i + 3] !== 0) {
-                    textParticle.push({
-                        index: i / 4,
-                        alpha: textData.data[i + 3] / 255,
-                    });
-                }
+        text.split(' ').forEach(item => {
+            if (/[a-zA-Z]/.test(item)) {
+                this.text.push(item);
+            } else {
+                this.text.push(...item.split(''));
             }
+        });
+    }
 
-            const color = Math.random() * 360;
-            for (let i = 0; i < particles.length; i++) {
-                let option = {
-                    random: true,
-                    delay: Math.random() * 1000,
-                    color,
-                    alpha: 1,
-                };
-                if (i < textParticle.length) {
-                    const { index, alpha } = textParticle[i];
-                    option.random = false;
-                    option.alpha = alpha;
-                    option.x = (index % textData.width) * this.cvsRatio;
-                    option.y = parseInt(index / textData.width) * this.cvsRatio;
-                }
-                particles[i].update(option);
+    _update() {
+        const { particles, textCvsCtx: cvsCtx } = this;
+        const { width, height } = this.textCvs;
+
+        cvsCtx.clearRect(0, 0, width, height);
+        cvsCtx.fillStyle = '#000000';
+        cvsCtx.font = `700 ${width / 6.4}px Helvetica`;
+        cvsCtx.textAlign = 'center';
+        cvsCtx.textBaseline = 'middle';
+        cvsCtx.fillText(this.text.shift(), width / 2, height / 2);
+
+        const textData = (this.textData = cvsCtx.getImageData(0, 0, width, height));
+
+        // 组成文字的粒子
+        const textParticle = [];
+        for (let i = 0; i < textData.data.length; i += 4) {
+            if (textData.data[i + 3] !== 0) {
+                textParticle.push({ index: i / 4, alpha: textData.data[i + 3] / 255 });
             }
-
-            // 一段时间后自动散开
-            setTimeout(() => {
-                const color = Math.random() * 360;
-                for (let i = 0; i < particles.length; i++) {
-                    particles[i].update({
-                        random: true,
-                        color,
-                        alpha: 1,
-                    });
-                }
-            }, 5000);
         }
+
+        // 设置粒子运动目标
+        const color = this.randomColor;
+        for (let i = 0; i < particles.length; i++) {
+            let option = {
+                random: true,
+                delay: this.randomDelay,
+                duration: this.randomDuration,
+                color,
+                alpha: this.randomAlpha,
+            };
+            if (i < textParticle.length) {
+                const { index, alpha } = textParticle[i];
+                option.random = false;
+                option.alpha = alpha;
+                option.x = (index % textData.width) * this.cvsRatio;
+                option.y = parseInt(index / textData.width) * this.cvsRatio;
+            }
+            particles[i].update(option);
+        }
+
+        // 一段时间后自动散开
+        this._spreadTimer = setTimeout(() => {
+            const color = this.randomColor;
+            for (let i = 0; i < particles.length; i++) {
+                particles[i].update({
+                    random: true,
+                    delay: this.randomDelay,
+                    duration: this.randomDuration,
+                    color,
+                    alpha: this.randomAlpha,
+                });
+            }
+
+            setTimeout(() => {
+                this._spreadTimer = null;
+                if (this.text.length === 0) {
+                    this.text.push(this.randomText);
+                }
+                this._update();
+            }, 3000);
+        }, 6000);
     }
 
     _onEnterFrame() {
@@ -268,19 +296,47 @@ class ParticleText {
     }
 }
 
-const particleText = new ParticleText(
-    cvs,
-    // '88:88:88'
-    new Date().toTimeString().match(/^.*(?=GMT)/g)[0]
-);
+const particleText = new ParticleText(cvs);
 
-let lastTime = Date.now();
-requestAnimationFrame(function draw() {
-    if (Date.now() - lastTime > 7000) {
-        lastTime = Date.now();
-        particleText.updateText(
-            new Date().toTimeString().match(/^.*(?=GMT)/g)[0]
-        );
-    }
-    requestAnimationFrame(draw);
-});
+(function () {
+    const inputContainer = document.querySelector('.input-container');
+    const inputContent = inputContainer.querySelector('.input-content');
+    const inputMask = inputContent.querySelector('.mask');
+    const input = inputContent.querySelector('input');
+
+    let inputContainerVisible = false;
+    let inputContentVisible = false;
+
+    window.addEventListener('keyup', function (e) {
+        if (e.code === 'Enter' && !inputContainerVisible) {
+            inputContainerVisible = true;
+            inputContainer.style.display = 'block';
+            inputContentVisible = true;
+            inputContent.classList.add('show');
+            input.focus();
+        }
+    });
+
+    input.addEventListener('keyup', function (e) {
+        if (e.code === 'Enter') {
+            inputContentVisible = false;
+            inputContent.classList.replace('show', 'hide');
+            input.blur();
+        }
+    });
+
+    input.addEventListener('blur', function (e) {
+        if (input.value.trim()) {
+            particleText.updateText(input.value.trim());
+        }
+    });
+
+    inputMask.addEventListener('animationend', function (e) {
+        if (!inputContentVisible) {
+            inputContainerVisible = false;
+            inputContainer.style.display = 'none';
+            inputContent.classList.remove('hide');
+            input.value = '';
+        }
+    });
+})();
