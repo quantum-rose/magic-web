016ae64c5db5fb231c19d396313d275e2b0587df
quantum rose <cui-haojie@outlook.com>
2020-12-07 16:25:53 +0800

    更新babel配置

--- 25-SVG-gradient/script.babel.js
+++ 25-SVG-gradient/script.babel.js
@@ -1,4 +1,1 @@
-"use strict";
-
-document.querySelector('svg').setAttribute('viewBox', "0 0 ".concat(window.innerWidth, " ").concat(window.innerHeight));
-//# sourceMappingURL=script.babel.js.map
+"use strict";document.querySelector("svg").setAttribute("viewBox","0 0 ".concat(window.innerWidth," ").concat(window.innerHeight));
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     presets: ['@babel/preset-env'],
     plugins: ['@babel/plugin-proposal-class-properties'],
+    ignore: ['node_modules', 'libs', 'babel.config.js', '.git', '**/*.babel.js'],
+    minified: true,
+    comments: false,
+    sourceMaps: true,
 };
