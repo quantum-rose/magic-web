0c9091b94cef94d0a12a8a1c92154342ba91c03e
崔豪杰 <cui-haojie@qq.com>
2025-04-08 16:12:07 +0800

    chore: 更新构建工具和方式

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     presets: [['@babel/preset-env', { modules: false }]],
-    plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-private-methods'],
     ignore: ['node_modules', 'libs', 'babel.config.js', '.git', '**/*.babel.js'],
     minified: true,
     comments: false,
-    sourceMaps: 'inline',
 };
